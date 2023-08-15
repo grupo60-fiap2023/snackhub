0ee82a6697ac1e6a50cd0bf7141113b57ade4344
Igor Oliveira <igor_villanova@hotmail.com>
2023-08-15 16:20:52 -0300

    Alterando o caminho dos arquivos do K8s, e criando services e deployments para a aplicação.

---
+++ Kubernetes/Init App.bat
@@ -0,0 +1,2 @@
+kubectl apply -f Deployments\deployment-app.yml
+kubectl apply -f Services\svc-app.yml
---
+++ Kubernetes/Init DB.bat
@@ -0,0 +1,5 @@
+kubectl delete service,deployments,persistentvolumeclaim,configmap --all
+kubectl apply -f PersistentVolumeClaim\pvc-db.yml
+kubectl apply -f ConfigMap\configmap-db.yml
+kubectl apply -f Deployments\deployment-db.yml
+kubectl apply -f Services\svc-db.yml

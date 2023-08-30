2b737064a3183c47269f3ffe6dc601fd7dde876f
Igor Oliveira <igor_villanova@hotmail.com>
2023-08-30 20:03:32 -0300

    Adicionado NLB ao Load Balance. Adicionado arquivo secrets

--- Kubernetes/Init DB.bat
+++ Kubernetes/Init DB.bat
@@ -1,4 +1,5 @@
-kubectl delete service,deployments,persistentvolumeclaim,configmap --all
+kubectl delete service,deployments,persistentvolumeclaim,configmap,secrets --all
+kubectl apply -f Secrets\secrets.yml
 kubectl apply -f PersistentVolumeClaim\pvc-db.yml
 kubectl apply -f ConfigMap\configmap-db.yml
 kubectl apply -f Deployments\deployment-db.yml
